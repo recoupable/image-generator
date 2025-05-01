16142965a38218158f560f3fe3bf1377d3b06b70
Sweets Sweetman <sweetmantech@gmail.com>
2025-04-30 21:07:34 -0300

    gpt-image-1 - MiniKit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+
 import { ImageGenerator } from "@/components/image-generator";
+import { useMiniKit } from "@coinbase/onchainkit/minikit";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { setFrameReady, isFrameReady } = useMiniKit();
+
+  // The setFrameReady() function is called when your mini-app is ready to be shown
+  useEffect(() => {
+    if (!isFrameReady) {
+      setFrameReady();
+    }
+  }, [setFrameReady, isFrameReady]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4 md:p-24">
       <div className="w-full max-w-3xl space-y-6">
