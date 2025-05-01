c6059da38fe23d3967e0c83a6a0f9ceb17757e95
Sweets Sweetman <sweetmantech@gmail.com>
2025-04-30 21:10:38 -0300

    well-known/farcaster.json and generateMetadata.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,27 @@ import { Inter } from "next/font/google";
 import { MiniKitProvider } from "@/providers/MiniKitProvider";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "AI Image Generator",
-  description: "Generate images using AI with the Vercel AI SDK",
+export const generateMetadata = (): Metadata => {
+  return {
+    title: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+    description: `${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME} - A MiniKit App`,
+    other: {
+      "fc:frame": JSON.stringify({
+        version: process.env.NEXT_PUBLIC_VERSION,
+        imageUrl: process.env.NEXT_PUBLIC_IMAGE_URL,
+        button: {
+          title: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME}`,
+          action: {
+            type: "launch_frame",
+            name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+            url: URL,
+            splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE_URL,
+            splashBackgroundColor: `#${process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR}`,
+          },
+        },
+      }),
+    },
+  };
 };
 
 export default function RootLayout({
